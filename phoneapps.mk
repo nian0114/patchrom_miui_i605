4bfeb492e2c8decdc77f05214a3e0ea273858375
nian0114 <268078545@qq.com>
2015-08-18 13:10:16 +0800

    Upload updates

---
+++ phoneapps.mk
@@ -0,0 +1,38 @@
+local-phone-apps = $(private-phone-apps)
+local-phone-priv-apps = $(private-phone-priv-apps)
+
+private-phone-apps := \
+BasicDreams \
+BluetoothExt \
+CertInstaller \
+DocumentsUI \
+SamsungServiceMode \
+HTMLViewer \
+KeyChain \
+Nfc \
+PacProcessor \
+PhotoTable \
+PrintSpooler \
+PicoTts \
+Stk \
+UserDictionaryProvider \
+VZWAPNLib \
+vzwapnpermission \
+VZWAPNService \
+WAPPushManager 
+
+private-phone-priv-apps := \
+BackupRestoreConfirmation \
+FusedLocation \
+InputDevices \
+DefaultContainerService \
+DownloadProvider \
+ExternalStorageProvider \
+MediaProvider \
+OneTimeInitializer \
+ProxyHandler \
+SharedStorageBackup \
+Shell \
+Tag \
+VpnDialogs \
+WallpaperCropper 
