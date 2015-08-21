317048481bc30da764e47e770dc8220bfeb7817b
nian0114 <268078545@qq.com>
2015-08-21 19:05:55 +0800

    Upload updates

--- customize_miui_app.sh
+++ customize_miui_app.sh
@@ -40,17 +40,12 @@ function mergyXmlPart() {
 	done
 }
 
-if [ $1 = "BarcodeScanner" ];then
-        rm  -rf out/$1/smali/com/zxing/camera/CameraManager\$FlashAsyncTask.smali
-	applyPatch $1 $2
-fi
-
 if [ $1 = "MiuiSystemUI" ];then
-	applyPatch $1 $2
+    applyPatch $1 $2
 fi
 
 if [ $1 = "Music" ];then
-	applyPatch $1 $2
+    applyPatch $1 $2
 fi
 
 if [ $1 = "miuisystem" ];then
